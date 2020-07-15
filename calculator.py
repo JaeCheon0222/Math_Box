bd6735009f1863952b6faffc72f63ac0d208e015
jaecheon <jaecheon0222@gmail.com>
2020-07-15 16:57:53 +0900

    Revert "Add multifly function"
    This reverts commit 6a7002b97901385dbc94569da3304437e3e9cdc2.

--- calculator.py
+++ calculator.py
@@ -3,6 +3,7 @@ def add(a, b):
   	return a+b
 
 def subtract(a, b):
+<<<<<<< HEAD
   	return a-b
 
 def multiply(a, b):
@@ -10,3 +11,6 @@ def multiply(a, b):
 
 def divide_new(a, b):
 	return a/b
+=======
+  return a-b
+>>>>>>> parent of 6a7002b... Add multifly function
